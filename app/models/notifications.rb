8a9530af4f8675e29cc802fa5245723c1b6bd77d
Wayne Graham <wayne.graham@virginia.edu>
2011-08-03 21:20:40 -0400

    updated notification model to use rails3 ActionMailer

--- app/models/notifications.rb
+++ app/models/notifications.rb
@@ -1,17 +1,9 @@
 class Notifications < ActionMailer::Base
 
-  QUESTION_RECIPIENTS = %W(bmp7e@virginia.edu)
+  default :to => %W(bmp7e@virginia.edu)
 
   def question(email_params, sent_at = Time.now)
-
-    # puts "Sending mail to: #{QUESTION_RECIPIENTS.inspect}"
-
-    mail(:to => QUESTION_RECIPIENTS, :from => email_params[:from],:subject => email_params[:subject])
-    # from email_params[:address]
-    # sent_on sent_at
-    # allows access to @message and @sender_name
-    # in view
-    # body :message => email_params[:body], :sender_name => email_params[:name]
+    mail(:from => email_params[:from],:subject => email_params[:subject])
   end
 
 end
