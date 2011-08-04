e14795e383213c5cc797b38052ae53c69bee4292
Wayne Graham <wayne.graham@virginia.edu>
2011-08-03 20:58:56 -0400

    upgraded session store to rails3

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,10 +4,16 @@
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_mind_is_a_metaphor_session',
-  :secret      => '3c7689f0f6c825a474889c9306cf524a0c91b9560f7778be060ab45208374b776310e0be41088407cb0b1483e2052df5750d50d8da0400bb1f4ceb047f6df118'
-}
+
+Rails.application.config.session_store :cookie_store, 
+  :key => '_mind_is_a_metaphor_session'
+
+# TODO: cleanup
+# Rails 2.3.x style
+# ActionController::Base.session = {
+#   :key         => '_mind_is_a_metaphor_session',
+#   :secret      => '3c7689f0f6c825a474889c9306cf524a0c91b9560f7778be060ab45208374b776310e0be41088407cb0b1483e2052df5750d50d8da0400bb1f4ceb047f6df118'
+# }
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
