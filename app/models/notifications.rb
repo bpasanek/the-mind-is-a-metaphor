163aa47e0957986f96d89c21bfec6cfc3a8c5cc7
Wayne Graham <wayne.graham@virginia.edu>
2011-08-03 21:08:29 -0400

    upgraded session store to rails3

--- app/models/notifications.rb
+++ app/models/notifications.rb
@@ -1,18 +1,17 @@
 class Notifications < ActionMailer::Base
-  
+
   QUESTION_RECIPIENTS = %W(bmp7e@virginia.edu)
-  
+
   def question(email_params, sent_at = Time.now)
-    
-    puts "Sending mail to: #{QUESTION_RECIPIENTS.inspect}"
-    
-    subject "[The Mind is a Metaphor] " << email_params[:subject]
-    recipients QUESTION_RECIPIENTS #TODO: change to Brad's before going live
-    from email_params[:address]
-    sent_on sent_at
+
+    # puts "Sending mail to: #{QUESTION_RECIPIENTS.inspect}"
+
+    mail(:to => QUESTION_RECIPIENTS, :from => email_params[:from],:subject => email_params[:subject])
+    # from email_params[:address]
+    # sent_on sent_at
     # allows access to @message and @sender_name
     # in view
-    body :message => email_params[:body], :sender_name => email_params[:name]
+    # body :message => email_params[:body], :sender_name => email_params[:name]
   end
 
 end
